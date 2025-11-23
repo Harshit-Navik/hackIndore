74905ece5fabde5a5d2d7cffd46e53b57a406f20
Krishna <kp8353156@gmail.com>
2025-11-23 23:55:21 +0530

    Resolving issues in mainfile

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { registerUser, loginUser, getUserProfile } from "../controllers/authController.js";
+import { registerUser, loginUser } from "../controllers/authController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/me", authMiddleware, getUserProfile);
 
 export default router;
